d1c4de3a2541afa71a001e4e3d9d36c84f0212fe
asnaweevixvify <asnaweeezor@gmail.com>
2025-05-30 17:43:58 +0700

    style

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,12 +5,9 @@ import { Link } from 'react-router-dom'
 function Nav(props) {
   return (
     <div className="nav-container">
-        <h2 className='nav-text'> Cart-System-React</h2>
-        <div className="search-nav">
           <Link to='/'><i className="fa-solid fa-house fa-2xl"></i></Link>
-          <input type='text' onInput={textType}></input>
+          <input type='text' placeholder='ค้นหาอาหาร' onInput={textType}></input>
           <Link to='/cart'><i className="fa-solid fa-cart-shopping fa-2xl"></i></Link>
-        </div>
     </div>
   )
   function textType(e){
